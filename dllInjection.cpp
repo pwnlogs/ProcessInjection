0d810beb728e2df1fea489459e93a12c44756586
Jithin Pavithran <jithinpavithran96@gmail.com>
2018-09-03 17:31:12 -0700

    dll injection working on Windows10

--- dllInjection.cpp
+++ dllInjection.cpp
@@ -1,17 +1,6 @@
 #include <Windows.h>
 #include <iostream>
-#include <filesystem>
-#include <atlconv.h>
 #include <TlHelp32.h>
-#include <tchar.h>
-#include <Winternl.h>
-#include <fstream>
-#include <wchar.h>
-#include <future>
-
-#define RTN_OK 0
-#define RTN_USAGE 1
-#define RTN_ERROR 13
 
 using namespace std;
 
@@ -20,7 +9,7 @@ BOOL Dll_Injection(const char* dll_name, const char* processname){
 	GetFullPathName(dll_name, MAX_PATH, lpdllpath, nullptr);
 	cout<<"[i] full path: "<<lpdllpath<<endl;
 
-	/* Snapshot of processes */
+	/* Finding specified process */
 	DWORD processId{};
 	cout<<"[+] creating process snapshot\n";
 	auto snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0); 
@@ -30,7 +19,6 @@ BOOL Dll_Injection(const char* dll_name, const char* processname){
 	}
 	cout<<"[+] created process snapshot\n\n";
 	PROCESSENTRY32 pe{};
-	
 	pe.dwSize = sizeof PROCESSENTRY32;
 	BOOL processFound = FALSE;
 	if(Process32First(snapshot, &pe)==FALSE){
@@ -60,17 +48,18 @@ BOOL Dll_Injection(const char* dll_name, const char* processname){
 		cout<<"[!] process not found"<<endl;
 		return FALSE;
 	}
-	/* Put the content to remote process memory */
+
+	/* Write full path of dll to victim process's memory */
 	auto size = strlen(lpdllpath)*sizeof(TCHAR); // len of dll's path
 	auto victimProcess = OpenProcess(PROCESS_ALL_ACCESS, 0, processId);
 	if(victimProcess==NULL){
 		cout<<"failed to get handle for process: "<<processId<<endl;
 		return FALSE;
 	}
-	cout<<"[+] Obtained handle to victim(host) process"<<endl;
-	auto baseAddressInVictimProcess = VirtualAllocEx(
+	cout<<"[+] obtained handle to victim(host) process"<<endl;
+	LPVOID baseAddressInVictimProcess = VirtualAllocEx(
 		victimProcess,				// handle for victim process
-		nullptr,					// let function decide where to allocate
+		NULL,						// let function decide where to allocate
 		size,						// size to allocate (size of dll's path)
 		MEM_COMMIT | MEM_RESERVE,	// commit as well as reserve the addresses
 		PAGE_READWRITE);			// enable only read-write (no execution needed)
@@ -79,48 +68,70 @@ BOOL Dll_Injection(const char* dll_name, const char* processname){
 		return FALSE;
 	}
 	cout<<"[+] allocated memory in victim(host) process"<<endl;
+	cout<<"[i] starting addess: 0x"<<baseAddressInVictimProcess<<endl;
 	if(WriteProcessMemory(
 		victimProcess,
 		baseAddressInVictimProcess,
 		lpdllpath,
 		size,
-		nullptr)==0){
+		NULL)==0){
 		cout<<"[!] write to victim process memory failed"<<endl;
 		return FALSE;
 	}
 	cout<<"[+] write to victim process memory successful"<<endl;
+
+	/* Creating remote thread(in victim process)
+	   with a call to LoadLibrary() to load dll to victim process */
 	auto kernel32 = GetModuleHandle("kernel32.dll");
 	if(kernel32==NULL){
 		cout<<"[!] undable to find kernel32!"<<endl;
 		return FALSE;
 	}
 	cout<<"[+] obtained handle to kernel32"<<endl;
-	auto loadLibAddr = GetProcAddress(kernel32, "LoadLibraryW");
+	
+	auto loadLibAddr = GetProcAddress(kernel32, "LoadLibraryA");
 	if(loadLibAddr==NULL){
-		cout<<"[!] unable to find function 'LoadLibraryW'"<<endl;
-		if((loadLibAddr=GetProcAddress(kernel32, "LoadLibraryA"))==NULL){
-			cout<<"[!] failed to find LoadLibraryA as well!"<<endl;
-			return FALSE;
-		}
-		cout<<"[+] but LoadLibraryA was found!"<<endl;
+		cout<<"[!] failed to find LoadLibrary"<<endl;
+		return FALSE;
 	}
-	auto remoteThreadId = CreateRemoteThread(
+	cout<<"[i] LoadLibraryA address: "<<loadLibAddr<<endl;
+
+	DWORD remoteThreadId = -1;
+	HANDLE remoteThreadHandle = CreateRemoteThread(
 		victimProcess,
-		nullptr,
+		NULL,
 		0,
 		reinterpret_cast<LPTHREAD_START_ROUTINE>(loadLibAddr),
 		baseAddressInVictimProcess,
-		NULL,
-		nullptr);
-	if(remoteThreadId==NULL){
+		// CREATE_SUSPENDED,
+		0,
+		&remoteThreadId);
+	if(remoteThreadHandle==NULL){
 		cout<<"[!] failed to create remote process"<<endl;
 		return FALSE;
 	}
-	cout<<"[+] remote thread started"<<endl;
-	WaitForSingleObject(remoteThreadId, INFINITE);
+	cout<<"[+] remote thread created"<<endl;
+	cout<<"[i] id: 0x"<<std::hex<<remoteThreadId<<std::dec<<"("<<remoteThreadId<<")"<<std::dec<<endl;
+	// Sleep(15000);
+	// ResumeThread(remoteThreadHandle);
+	// cout<<"[+] remote thread started"<<endl;
+	DWORD threadStatus = WaitForSingleObject(remoteThreadHandle, INFINITE);
+	if(threadStatus!=WAIT_OBJECT_0){
+		cout<<"[!] thread termination status unkown"<<endl;
+		return FALSE;
+	}
 	cout<<"[+] remote thread execution completed"<<endl;
+	DWORD returnValue=-1;
+	if(GetExitCodeThread(remoteThreadHandle, &returnValue)==0){
+		cout<<"[!] failed to retrieve return status"<<endl;
+		return FALSE;
+	}
+	cout<<"[+] return value: "<<returnValue<<endl;	
+
+	/* Cleaning up */
 	cout<<"[+] cleaning up..."<<endl;
 	CloseHandle(victimProcess);
+	CloseHandle(remoteThreadHandle);
 	VirtualFreeEx(victimProcess, baseAddressInVictimProcess, size, MEM_RELEASE);
 	cout<<"[+] injection SUCCESSFUL";
 	return TRUE;
--- injectDll.cpp
+++ injectDll.cpp
@@ -3,11 +3,32 @@
 
 #pragma comment(lib,"user32.lib")
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+BOOL APIENTRY DllMain(
+HANDLE hModule,// Handle to DLL module
+DWORD ul_reason_for_call,// Reason for calling function
+LPVOID lpReserved ) // Reserved
 {
-    MessageBox(NULL, "Hello", "From DLL", MB_OK);
-    return TRUE;
+	switch ( ul_reason_for_call )
+	{
+		case DLL_PROCESS_ATTACH:
+			// A process is loading the DLL.
+		    MessageBox(NULL, "Called due to dll load or process load", "injectDll", MB_OK);
+			break;
+		case DLL_THREAD_ATTACH:
+			// A process is creating a new thread.
+		    // MessageBox(NULL, "Called due to new thread creation", "injectDll", MB_OK);
+			break;
+		case DLL_THREAD_DETACH:
+			// A thread exits normally.
+		    // MessageBox(NULL, "Call due to thread termination", "injectDll", MB_OK);
+			break;
+		case DLL_PROCESS_DETACH:
+			// A process unloads the DLL.
+		    // MessageBox(NULL, "Called due to dll unload of process exit", "injectDll", MB_OK);
+			break;
+		default:
+			;
+			// MessageBox(NULL, "Called for unknown reason", "injectDll", MB_OK);
+	}
+	return TRUE;
 }
